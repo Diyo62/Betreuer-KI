5f401221186662eba67407ce9f803dc86a02389a
Diyo62 <kabakciserkan@web.de>
2026-02-19 00:53:06 +0100

    Remove font family style setting
    Remove font family style from document body

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-document.body.style.fontFamily = 'Arial, Helvetica, sans-serif';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />

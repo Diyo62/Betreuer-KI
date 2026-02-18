caae1e628b4b4606b60e4547c182b831412eeab5
Diyo62 <kabakciserkan@web.de>
2026-02-19 00:51:00 +0100

    Add font family style to document body
    Set the default font family for the document body.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+document.body.style.fontFamily = 'Arial, Helvetica, sans-serif';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
